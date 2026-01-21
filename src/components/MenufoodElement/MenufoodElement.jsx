73587264dd6e25f424e6f6bf49ee90133a384127
Kirill <kirilzhm@gmail.com>
2026-01-21 15:58:56 +0200

    small commit

---
+++ src/components/MenufoodElement/MenufoodElement.jsx
@@ -0,0 +1,14 @@
+import styles from "./MenufoodElement.module.css"
+
+const Menu =() => {
+    return (
+        <div className={styles.background}>
+            <p>Menu</p>
+            <div>
+                
+            </div>
+        </div>
+    )
+};
+
+export default Menu;
---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,14 @@
+import Menu from "../../components/menufoodElement/MenufoodElement";
+
+const HomePage = () => {
+    return (
+        // дивка для фона
+        <div> 
+            <div>
+                <Menu />
+            </div>
+        </div>
+    )
+};
+
+export default HomePage;
